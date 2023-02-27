14f6889a85523da8a196f6acd4e59944525e893e
Boris R <borisrozhkin@gmail.com>
2023-02-27 11:55:10 +0500

    Фабрику починил

--- src/main/kotlin/dt_heating.kt
+++ src/main/kotlin/dt_heating.kt
@@ -240,17 +240,22 @@ class DT(var temp: DT_temp, val type: DT_type, var ht: DT_heat_transfer, var cur
         }
     }
 }
-fun dt_ht_create(type: String): DT
+fun dt_ht_create(dt_type: String): DT
 {
-    var temp = DT_temp(293.0, 293.0, 293.0,293.0)
-    var hc = DT_heat_transfer(310.0,310.0,310.0,5.35,8.69,800.0,253.0,0.5,1.0)
-    if ("DT-0.6-1000" == type){
+    val temp = DT_temp(293.0, 293.0, 293.0,293.0)
+    var hc = DT_heat_transfer(310.0,310.0,310.0,5.35,8.69,800.0,293.0,0.5,1.0)
+    if (0 == dt_type.compareTo("DT-0.6-1000") || 0 == dt_type.compareTo("DT-0.2-1000") ){
         var type = DT_type(40.0,390.0,0.0172,288.0, 10.872,0.58, 70.0, 480.0, 0.283,24.3, 1670.0, 47.0, 480.0, 0.760000,0.3,1.3,0.86,0.258,0.9, 2000.0, "DT-0.6-1000")
         return DT(temp, type, hc, 0.0)
     }
-    else{
-        throw Exception("Not supported type $type")
+    else if ( 0 == dt_type.compareTo("DT-0.4-1500") ){
+        var type = DT_type(40.0,390.0,0.0172,288.0, 10.872,0.58, 70.0, 480.0, 0.283,24.3, 1670.0, 47.0, 480.0, 0.760000,0.3,1.3,0.86,0.258,0.9, 2000.0, "DT-0.2-1000")
+        return DT(temp, type, hc, 0.0)
     }
+    else{
+        throw Exception("Not supported type $dt_type")
+    }
+
 }
 /*
   fun calc_next() {
